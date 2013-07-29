a17a5990284e07f51e90d575256cb31340a99613
Matthew Weier O'Phinney <matthew@zend.com>
2013-07-29 16:57:41 -0500

    Updated configuration
    - Each item of configuration needs a "route" and a "response" section; the
      configuration in this example is for routing.

--- config/module.config.php
+++ config/module.config.php
@@ -2,9 +2,11 @@
 return array(
     'zf-rpc' => array(
         array(
-            'methods' => 'GET',
-            'url' => '/api-first-example/hello',
-            'dispatchable' => 'ZFApiFirstExample\Controller\MyRpcController::hello'
+            'route' => array(
+                'methods'      => 'GET',
+                'url'          => '/api-first-example/hello',
+                'dispatchable' => 'ZFApiFirstExample\Controller\MyRpcController::hello'
+            ),
         )
     )
 );
