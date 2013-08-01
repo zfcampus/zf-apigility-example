d6b74e5d2c88800cf131123ec0c702bc7fcc4d25
Ralph Schindler <ralph@ralphschindler.com>
2013-08-01 08:54:04 -0500

    Split RPC into smaller config + route config

--- config/module.config.php
+++ config/module.config.php
@@ -1,12 +1,21 @@
 <?php
 return array(
     'zf-rpc' => array(
-        array(
-            'route' => array(
-                'methods'      => 'GET',
-                'url'          => '/api-first-example/hello',
-                'dispatchable' => 'ZFApiFirstExample\Controller\MyRpcController::hello'
+        'HelloWorld' => array(
+            'callable' => 'ZFApiFirstExample\Controller\MyRpcController::hello'
+        ),
+    ),
+    'router' => array(
+        'routes' => array(
+            'rpc-hello-world' => array(
+                'type' => 'Segment',
+                'options' => array(
+                    'route' => '/api-first-example/hello',
+                    'defaults' => array(
+                        'controller' => 'HelloWorld',
+                    ),
+                ),
             ),
-        )
-    )
+        ),
+    ),
 );
