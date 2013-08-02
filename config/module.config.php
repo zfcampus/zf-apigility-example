acce34c28f7a5c11046f09f7085e5d7c7ca3929f
Ralph Schindler <ralph@ralphschindler.com>
2013-08-02 13:25:52 -0500

    Updated config for HAL and RPC

--- config/module.config.php
+++ config/module.config.php
@@ -1,7 +1,12 @@
 <?php
 return array(
     'zf-rpc' => array(
-        'HelloWorld' => 'ZFApiFirstExample\Controller\MyRpcController::hello',
+	'HelloWorld' => 'ZFApiFirstExample\Controller\MyRpcController::hello'
+    ),
+    'zf-content-negotiation' => array(
+	'controllers' => array(
+	    'HelloWorld' => 'HalJson'
+	)
     ),
     'router' => array(
         'routes' => array(
